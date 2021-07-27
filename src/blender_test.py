2d58496b8bf803316db4a913cc5230543f8efac1
Noah Price <noah.price.237@gmail.com>
2021-07-27 15:58:19 -0400

    Fix issue where base object was repeatedly changed by subsequent objects Fix issue where Array modifier was not being applied

--- src/blender_test.py
+++ src/blender_test.py
@@ -9,23 +9,28 @@ mod = active_obj.modifiers.new("Array", "ARRAY")
 mod.relative_offset_displace = [0, 0, 1.1]
 mod.count = 4
 
-bpy.ops.object.modifier_apply(modifier='ARRAY')
+bpy.ops.object.modifier_apply(modifier='Array')
 # Separate by loose ends
 bpy.ops.mesh.separate(type='LOOSE')
 
 # TODO Make sure list isn't empty
-tracks = active_obj.animation_data.nla_tracks
-prev_key = tracks.strips[0].action.fcurves[0].keyframe_points.co
-for obj in bpy.context.selected_objects:
-    # print(obj.name)
-    if obj == bpy.context.active_object:
 
-        print(prev_key)
+# need an index for each keyframe
+# currently only returns keyframe[0]
+
+prev_key = active_obj.animation_data.nla_tracks[0].strips[0].action.fcurves[0].keyframe_points[0].co
+
+for obj in bpy.context.selected_objects:
+    print(obj.name)
+    if obj == bpy.context.active_object:
+        continue
     else:
-        for track in tracks:
+        for track in obj.animation_data.nla_tracks:
             for strip in track.strips:
                 action = strip.action
                 for fcu in action.fcurves:
                     for keyframe in fcu.keyframe_points:
                         keyframe.co.x = prev_key.x + 2
                         keyframe.co.y = prev_key.y + 2
+                        print(keyframe.co)
+                        print(prev_key)
