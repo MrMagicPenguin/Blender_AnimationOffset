Fix uv rotation using numpy and Matrix

src/Rakas/uvoffset.py
@@ -2,20 +2,14 @@
 # https://blender.stackexchange.com/questions/75061/scale-uv-map-script
 
 import bpy
-from mathutils import Vector
+from mathutils import Vector, Matrix
+import numpy as np
 from math import pi, sin, cos, atan2, hypot
 
 
 def to_cartesian(polar_coord):
     length, angle = polar_coord.x, polar_coord.y
     return Vector((length * cos(angle), length * sin(angle)))
-
-
-def to_cartesian_pivot(polar_coord, angle, pivot):
-    # ? does this change from polar to cartesian? does this formula assume cartesian coords?
-    x_rotated = ((polar_coord.x - pivot.x) * cos(angle)) - ((polar_coord.y - pivot.y) * sin(angle)) + pivot.x
-    y_rotated = ((polar_coord.x - pivot.x) * sin(angle)) - ((polar_coord.y - pivot.y) * cos(angle))n + pivot.y
-    return Vector((x_rotated, y_rotated))
 
 
 def to_polar(vector):
@@ -26,17 +20,20 @@ def to_polar(vector):
 
 # Get object and UV map given their names
 # !rm
-def GetObjectAndUVMap(objName, uvMapName):
-    try:
-        obj = bpy.context.active_object
+def GetObjectAndUVMap(obj, uvMapName):
 
+    if obj is not None:
         if obj.type == 'MESH':
             uvMap = obj.data.uv_layers[uvMapName]
             return obj, uvMap
-    except:
-        print("No Mesh Selected!")
+
+    print("No Mesh Selected!")
 
     return None, None
+
+
+def GetAspectRatio(obj):
+    return obj.dimensions.y / obj.dimensions.x
 
 
 # Scale a 2D vector v, considering a scale s and a pivot point p
@@ -47,19 +44,18 @@ def Scale2D(v, s, p):
             p[1] + s[1] * (v[1] - p[1]))
 
 
-def Rotate2D(v, r, p):
-    # get distance of vector
-    # convert v, p to polar coords
-    # add/subtract r from v
-    # convert to cartesian
-    # return Vector2 coords
-    # * thanks to Mark Booth
-    # https://stackoverflow.com/questions/620745/c-rotating-a-vector-around-a-certain-point
+def Rotate2D(obj, angle, pivot, uvs):
+    aspect = GetAspectRatio(obj)
+    R = Matrix((
+        (np.cos(angle), np.sin(angle) / aspect),
+        (-aspect * np.sin(angle), np.cos(angle))
+    ))
 
-    v_polar = to_polar(v)
-    v_rotated_polar = Vector((v_polar.x, v_polar.y + r))
-    v_prime = to_cartesian_pivot(v_rotated_polar, r, p)
-    return v_prime
+    uvs = np.dot(
+        uvs.reshape((-1, 2)) - pivot,
+        R) + pivot
+
+    return uvs
 
 
 def Translate2D(v, t):
@@ -73,10 +69,17 @@ def ScaleUV(uvMap, scale, pivot):
         uvMap.data[uvIndex].uv = Scale2D(uvMap.data[uvIndex].uv, scale, pivot)
 
 
-def RotateUV(uvMap, rotation, pivot):
-    for uvIndex in range(len(uvMap.data)):
-        uvMap.data[uvIndex].uv = Rotate2D(uvMap.data[uvIndex].uv, rotation, pivot)
+def RotateUV(obj, angle, pivot):
+    # for uvIndex in range(len(uvMap.data)):
+    #    uvMap.data[uvIndex].uv = Rotate2D(uvMap.data[uvIndex].uv, angle, pivot)
+    # ? don't require the for loop here, this is handled by the foreach_get/set method
 
+    uvs = np.empty(2 * len(obj.data.loops))
+    uvs = obj.data.uv_layers.active.data.foreach_get("uv", uvs)
+    uvs = Rotate2D(obj, angle, pivot, uvs)
+
+    obj.data.uv_layers.active.data.foreach_set("uv", uvs.ravel())
+    obj.data.update()
 
 def TranslateUV(uvMap, translate):
     for uvIndex in range(len(uvMap.data)):
@@ -87,19 +90,20 @@ def TranslateUV(uvMap, translate):
 bpy.ops.object.mode_set(mode='OBJECT')
 
 # The names of the object and map
-objName = 'Cube'
 uvMapName = 'UVMap'
 
 # Defines the pivot and scale
 wpivot = Vector((0.5, 0.5))
 scale = Vector((2, 2))
-rotation = pi / 4
+rotation = pi / 4  # use numpy.radians(degrees) instead
 # * Mind that UV coords are in a 0-1 space; adding whole numbers to a vector will not change the pattern significantly.
 translate = Vector((1, 0))
 
 # Get the object from names
-obj, uvMap = GetObjectAndUVMap(objName, uvMapName)
+obj = bpy.context.active_object
+uvMap = GetObjectAndUVMap(obj, uvMapName)
+
 
 # If the object is found, scale its UV map
 if obj is not None:
-    RotateUV(uvMap, rotation, wpivot)
+    RotateUV(obj, rotation, wpivot)